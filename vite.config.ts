d28786ef2129814d5a598e3a96c6ed0be68c1ade
00kortex00 <koesuper228@gmail.com>
2026-02-15 14:40:28 +0200

    added aliases for react to use preact instead. (useful for libraries we'll use to force them use preact)

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,10 @@ import { name, version } from "./package.json";
 export default defineConfig({
 	resolve: {
 		alias: {
+			react: "preact/compat",
+			"react-dom": "preact/compat",
+			"react-dom/test-utils": "preact/test-utils",
+			"react/jsx-runtime": "preact/jsx-runtime",
 			"@": `${path.resolve(__dirname, "src")}`,
 		},
 	},
