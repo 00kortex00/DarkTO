8c55da42789ddfe27f4230569a74244e40918bdb
00kortex00 <koesuper228@gmail.com>
2026-02-15 15:07:35 +0200

    prettier to all files

--- src/sidepanel/App.tsx
+++ src/sidepanel/App.tsx
@@ -1,18 +1,18 @@
-import crxLogo from '@/assets/crx.svg'
-import viteLogo from '@/assets/vite.svg'
-import HelloWorld from '@/components/HelloWorld'
-import './App.css'
+import crxLogo from "@/assets/crx.svg";
+import viteLogo from "@/assets/vite.svg";
+import HelloWorld from "@/components/HelloWorld";
+import "./App.css";
 
 export default function App() {
-  return (
-    <div>
-      <a href="https://vite.dev" target="_blank" rel="noreferrer">
-        <img src={viteLogo} className="logo" alt="Vite logo" />
-      </a>
-      <a href="https://crxjs.dev/vite-plugin" target="_blank" rel="noreferrer">
-        <img src={crxLogo} className="logo crx" alt="crx logo" />
-      </a>
-      <HelloWorld msg="Vite + Preact + CRXJS" />
-    </div>
-  )
+	return (
+		<div>
+			<a href="https://vite.dev" target="_blank" rel="noreferrer">
+				<img src={viteLogo} className="logo" alt="Vite logo" />
+			</a>
+			<a href="https://crxjs.dev/vite-plugin" target="_blank" rel="noreferrer">
+				<img src={crxLogo} className="logo crx" alt="crx logo" />
+			</a>
+			<HelloWorld msg="Vite + Preact + CRXJS" />
+		</div>
+	);
 }
